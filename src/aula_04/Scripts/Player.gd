e43f1339d9e759f5f1953f11ee003ceb4e51e4fa
Narto Junior <nartojunior@outlook.com>
2024-05-05 14:21:08 -0300

    chore(input): adiciona mapeamento de input no projeto.

--- src/aula_04/Scripts/Player.gd
+++ src/aula_04/Scripts/Player.gd
@@ -1,3 +1,23 @@
 extends CharacterBody2D
 
-@export var a:int
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var is_running:bool = false
+
+func _ready():
+    animation_player.play("idle")
+    pass
+
+func _process(delta:float) -> void:
+
+    if Input.is_action_just_pressed("ui_accept"):
+        if is_running:
+            animation_player.play("idle")
+            is_running = false
+            pass
+        else:
+            animation_player.play("run")
+            is_running = true  
+
+    pass
